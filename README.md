66ed63995c4a2812d3f88966c51c915949f6170b
Verl95 <dannybirrell7@gmail.com>
2026-01-21 13:14:31 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mcconnect-app2
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Verl95/mcconnect-app2)
---
+++ public/Service-worker.js
@@ -0,0 +1,4 @@
+// This allows the app to be installed.
+self.addEventListener('fetch', function(event) {
+  // Simple pass-through for now
+});
