b1344226706204b4a68d29104e00bcf0fe11939f
Verl95 <dannybirrell7@gmail.com>
2026-01-28 11:22:55 +0000

    chnage output to build

--- vite.cofig.js
+++ vite.cofig.js
@@ -1,4 +1,11 @@
-import {deineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-//https://vitejs.dev/config/export default defineConfig({plugins: [react()],})
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+  }
+})
+
